4c20305046e32e72646f6889e305973aa8e66da0
vymmf <vymmf@umsystem.edu>
2025-06-12 13:38:41 -0500

    Lab 1

---
+++ Lab 1/Main.cpp
@@ -0,0 +1,217 @@
+// Name: Vincent Yang
+// Program 2 - Distance
+/* Purpose:
+
+*/
+
+/*
+Pseudocode
+
+INPUT: 6 values; loop, manualInput, x1, y1, x2, y2
+OUTPUT: 
+Display Coordinates and Distance from Origin of each point (x,y), then tell the user which point is closer to the Origin.
+
+main():
+    WHILE (loop == 'Y')
+        CHAR INPUT manualInput
+        IF (manualInput == 'Y')
+            THEN INPUT x1
+            IF (10 > x1 > -10) or NOT Numeric
+                THEN WHILE (10 > x1 > -10) or NOT Numeric
+                    INPUT x1
+            INPUT y1
+            IF (10 > y1 > -10) or NOT Numeric
+                THEN WHILE (10 > y1 > -10) or NOT Numeric
+                    INPUT y1
+            INPUT x2
+            IF (10 > x2 > -10) or NOT Numeric
+                THEN WHILE (10 > x2 > -10) or NOT Numeric
+                    INPUT x2
+            INPUT y2
+            IF (10 > y2 > -10) or NOT Numeric
+                THEN WHILE (10 > y2 > -10) or NOT Numeric
+                    INPUT y2
+        ELSE
+            THEN x1 = randomInteger(MIN = -10, MAX = 10)
+            y1 = randomInteger(MIN = -10, MAX = 10)
+            x2 = randomInteger(MIN = -10, MAX = 10)
+            y2 = randomInteger(MIN = -10, MAX = 10)
+        
+        DOUBLE point1Distance = Power(Power(x1, 2) + Power(y1, 2), 0.5)
+        DOUBLE point2Distance = Power(Power(x2, 2) + Power(y2, 2), 0.5)
+
+        PRINT Point 1: (x1, y1)
+        PRINT Distance: point1Distance
+
+        PRINT Point 2: (x2, y2)
+        PRINT Distance: point2Distance
+
+        IF (point1Distance < point2Distance)
+            THEN PRINT Closest Point: point1Distance
+        ELSE
+            THEN PRINT Closest Point: point2Distance
+
+        CHAR INPUT loop
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x1, y1, x2, y2;
+    char manualInput = 'Y';
+    char loop = 'Y';
+
+    cout << "Welcome to your 2nd C++ program" << endl;
+    while (loop == 'Y')
+    {
+        cout << "Do you wish to enter the coordinates manually? If not, the program will generate random coordinates. (Enter Y if Yes)" << endl;
+        cin >> manualInput;
+
+        if (manualInput == 'Y')
+        {
+            cout << "Please enter the coordinates to be added (Format: x1, y1, x2, y2):" << endl;
+            while (true)
+            {
+                if (cin >> x1 >> y1 >> x2 >> y2)
+                {
+                    if ((10 >= x1) && (x1 >= -10) && (10 >= y1) && (y1 >= -10) && (10 >= x2) && (x2 >= -10) && (10 >= y2) && (y2 >= -10))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        cout << "Invalid Input, Try Again. (Must be between 10 and -10)" << endl;
+                    }
+                }
+                else
+                {
+                    cout << "Invalid Input, Try Again. (Must be Numeric)" << endl;
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                }
+            }
+            //     if (cin >> x1)
+            //     {
+            //         if ((10 >= x1) && (x1 >= -10))
+            //         {
+            //             break;
+            //         }
+            //         else
+            //         {
+            //             cout << "Invalid Input, Try Again. (Must be between 10 and -10)" << endl;
+            //         }
+            //     }
+            //     else
+            //     {
+            //         cout << "Invalid Input, Try Again. (Must be Numeric)" << endl;
+            //         cin.clear();
+            //         cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            //     }
+            // }
+            
+            // cout << "Please enter the 1st y coordinate:" << endl;
+            // while (true)
+            // {
+            //     if (cin >> y1)
+            //     {
+            //         if ((10 >= y1) && (y1 >= -10))
+            //         {
+            //             break;
+            //         }
+            //         else
+            //         {
+            //             cout << "Invalid Input, Try Again. (Must be between 10 and -10)" << endl;
+            //         }
+            //     }
+            //     else
+            //     {
+            //         cout << "Invalid Input, Try Again. (Must be Numeric)" << endl;
+            //         cin.clear();
+            //         cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            //     }
+            // }
+
+            // cout << "Please enter the 2nd x coordinate:" << endl;
+            // while (true)
+            // {
+            //     if (cin >> x2)
+            //     {
+            //         if ((10 >= x2) && (x2 >= -10))
+            //         {
+            //             break;
+            //         }
+            //         else
+            //         {
+            //             cout << "Invalid Input, Try Again. (Must be between 10 and -10)" << endl;
+            //         }
+            //     }
+            //     else
+            //     {
+            //         cout << "Invalid Input, Try Again. (Must be Numeric)" << endl;
+            //         cin.clear();
+            //         cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            //     }
+            // }
+
+            // cout << "Please enter the 2nd y coordinate:" << endl;
+            // while (true)
+            // {
+            //     if (cin >> y2)
+            //     {
+            //         if ((10 >= y2) && (y2 >= -10))
+            //         {
+            //             break;
+            //         }
+            //         else
+            //         {
+            //             cout << "Invalid Input, Try Again. (Must be between 10 and -10)" << endl;
+            //         }
+            //     }
+            //     else
+            //     {
+            //         cout << "Invalid Input, Try Again. (Must be Numeric)" << endl;
+            //         cin.clear();
+            //         cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            //     }
+            //}
+        }
+        else
+        {
+            x1 = rand() % 21 - 10;
+            y1 = rand() % 21 - 10;
+            x2 = rand() % 21 - 10;
+            y2 = rand() % 21 - 10;
+        }
+
+        // cout << "x1: " << x1 << endl;
+        // cout << "y1: " << y1 << endl;
+        // cout << "x2: " << x2 << endl;
+        // cout << "y2: " << y2 << endl;
+
+        double point1Distance = pow(pow(x1, 2) + pow(y1, 2), 0.5);
+        double point2Distance = pow(pow(x2, 2) + pow(y2, 2), 0.5);
+
+        cout << "\nPoint 1: (" << x1 << ',' << y1 << ")\n" << "Distance: " << point1Distance << endl << endl;
+        cout << "Point 2: (" << x2 << ',' << y2 << ")\n" << "Distance: " << point2Distance << endl << endl;
+
+        if (point1Distance < point2Distance)
+        {
+            cout << "Point 1 is closer to the origin." << endl << endl;
+        }
+        else if (point2Distance < point1Distance)
+        {
+            cout << "Point 2 is closer to the origin." << endl << endl;
+        }
+        else
+        {
+            cout << "Both points are of equal distance from the origin." << endl << endl;
+        }
+
+        cout << "Again? (Enter Y if Yes)" << endl;
+        cin >> loop;
+    }
+
+    return 0;
+}
