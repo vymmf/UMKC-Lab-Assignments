a33da21ffef0992bcaa4acdef928e5d3b654cee1
vymmf <vymmf@umsystem.edu>
2025-07-01 12:06:37 -0500

    Cleaning Code to match Problem + Added Comments explaining code

--- Midterm/Main.cpp
+++ Midterm/Main.cpp
@@ -10,129 +10,49 @@ to order more chocolate.
 #include <iomanip>
 using namespace std;
 
-double calculateShipping(int numPounds) // Calculates shipping costs based on branching
+// Displays a welcome message to the user
+void displayIntro()
 {
-    if (numPounds <= 10)
-    {
-        return 8.25;
-    }
-    else if (numPounds <= 20)
-    {
-        return 12.75;
-    }
-    else if (numPounds <= 50)
-    {
-        return 15.25;
-    }
-    else
-    {
-        return 19.75;
-    }
+    cout << "WELCOME TO MY CHOCOLATE FACTORY" << endl;
 }
 
-void printInventory(int largeInv, int mediumInv, int smallInv)
+// Displays the current/remaining inventory of large, medium, and small bars
+void displayInventory(int largeInv, int mediumInv, int smallInv)
 {
-    cout << "Chocolate Inventory\n" << "Large Bars: " << setw(4) << largeInv << endl << "Medium Bars: " << setw(4) << mediumInv << endl << "Small Bars: " << setw(4) << smallInv << endl;
+    cout << "Remaining Inventory\n" <<
+    "Small Bars: " << setw(4) << smallInv << endl <<
+    "Medium Bars: " << setw(4) << mediumInv << endl <<
+    "Large Bars: " << setw(4) << largeInv << endl
+    << endl << endl;
 }
 
-
-void orderInventory(int& largeInv, int& mediumInv, int& smallInv)
-{
-    int amount;
-    int invOfType;
-    char type;
-    string typeAsString;
-
-    char validTypes[3] = {'L', 'M', 'S'};
-
-    while (true)
-    {
-        cout << "Enter the number of bars needed: ";
-        if ((cin >> amount) && (amount > 0))
-        {
-            break;
-        }
-        else
-        {
-            cout << "\nWARNING: Input is not a valid positive integer. Command ignored." << endl;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        }
-    }
-
-    while (true)
-    {
-        cout << "Enter the type (L, M, S): ";
-        cin >> type;
-
-        if (toupper(type) == 'L')
-        {
-            typeAsString = "large";
-            largeInv += amount;
-            invOfType = largeInv;
-            break;
-        }
-        else if (toupper(type) == 'M')
-        {
-            typeAsString = "medium";
-            mediumInv += amount;
-            invOfType = mediumInv;
-            break;
-        }
-        else if (toupper(type) == 'S')
-        {
-            typeAsString = "small";
-            smallInv += amount;
-            invOfType = smallInv;
-            break;
-        }
-        else
-        {
-            cout << "\nWARNING: " << type << " is an invalid type. Command ignored." << endl;
-        }
-    }
-    // else
-    // {
-    //     typeAsString = ""
-    // }
-
-    cout << "An additional " << amount << ' ' << typeAsString << " bars ordered. New total of " << typeAsString << " bars = " << invOfType << endl;
-}
-
+// Fills an order by prompting the user to enter the total number of pounds of chocolate they would like, then optimizes the process by using large bars first, then medium bars, then small bars if necessary.
 void fillOrder(int& largeInv, int& mediumInv, int& smallInv, float largePrice, float mediumPrice, float smallPrice, int& orderNum)
 {
-    int numPounds;
-    int totalInv = (largeInv * 5) + (mediumInv * 3) + smallInv;
-    double taxRate = 0.072;
+    int numPounds; // User's input for how many pounds they want
+    int totalInv = (largeInv * 5) + (mediumInv * 3) + smallInv; // Measures the total inventory in pounds, large is 5 pounds, medium is 3 pounds, small is 1 pound.
 
-    double largeTotal;
-    double mediumTotal;
-    double smallTotal;
+    double largeTotal; // Amount of large bars that will be used in the order times the price; basically total cost of large bars
+    double mediumTotal; // Amount of medium bars that will be used in the order times the price; basically total cost of medium bars
+    double smallTotal; // Amount of small bars that will be used in the order times the price; basically total cost of small bars
 
-    double subTotalCost;
-    double totalCost;
+    double totalCost; // All fo the totals of large, medium, and small bars and adds them up.
 
-    double taxTotal;
-    double shipping;
-
-    // while (true)
-    // {
-    cout << "Please enter the number of pounds of chocolate you would like to order: ";
-    if ((cin >> numPounds) && (numPounds > 0))
+    cout << "Enter total number of pounds of chocolate you would like: ";
+    if ((cin >> numPounds) && (numPounds > 0)) // Checks if the user's input is a positive integer, if not a warning message is displayed.
     {
-        if (numPounds <= totalInv)
+        if (numPounds <= totalInv) // Checks if the user's input is equal to or less than the total inventory in pounds, if it's greater than the total inventory then a warning message is displayed.
         {
-            shipping = calculateShipping(numPounds);
-
-            int wantedLarge = numPounds / 5;
-            if (wantedLarge > largeInv)
+            int wantedLarge = numPounds / 5; // Amount of large bars needed to fulfill the order
+            if (wantedLarge > largeInv) // Checks if the amount of large bars needed exceeds the inventory for large bars, and if it is, then makes the needed amount equal to the current inventory (Basically uses up all available large bars)
             {
                 wantedLarge = largeInv;
             }
-            numPounds -= (wantedLarge * 5);
-            largeTotal = wantedLarge * largePrice;
-            largeInv -= wantedLarge;
+            numPounds -= (wantedLarge * 5); // Subtracts the amount of pounds needed from the user, this process makes it so the program doesn't count the amount that's already filled by the large bars
+            largeTotal = wantedLarge * largePrice; // already explained above
+            largeInv -= wantedLarge; // Subtracts the amount of needed large bars from the inventory, since they will be used for the order
 
+            // Same process as the large bars as above, but with a different weight value (3 pounds per bar instead of 5 pounds)
             int wantedMedium = numPounds / 3;
             if (wantedMedium > mediumInv)
             {
@@ -142,91 +62,59 @@ void fillOrder(int& largeInv, int& mediumInv, int& smallInv, float largePrice, f
             mediumTotal = wantedMedium * mediumPrice;
             mediumInv -= wantedMedium;
 
+            // Remaining amount of chocolate pounds needed will be fulfilled with small bars, since they are 1 pound each.
             int wantedSmall = numPounds;
             smallTotal = wantedSmall * smallPrice;
             smallInv -= wantedSmall;
 
-            subTotalCost = largeTotal + mediumTotal + smallTotal;
+            totalCost = largeTotal + mediumTotal + smallTotal; // Already explained above
 
-            taxTotal = taxRate * subTotalCost;
+            // Displays the order and the cost/total cost of each part of the order (small, medium, large bars)
+            cout << fixed << setprecision(2) << setw(10); // fixed makes it so the numbers don't show up in fixed point notation and not scientific notation; setprecision sets the number of sig figs a number with decimals will have; setw() sets the width of the message.
+            cout << "Your order has been filled:" << endl;
+            cout << "Small Bars:  " << setw(4) << wantedSmall << " = " <<  setw(9) << smallTotal << endl;
+            cout << "Medium Bars: " << setw(4) << wantedMedium << " = " << setw(9) << mediumTotal << endl;
+            cout << "Large Bars:  " << setw(4) << wantedLarge << " = " << setw(9) << largeTotal << endl;
+            cout << "Total Cost:  " << setw(16) << totalCost << endl << endl;
 
-            totalCost = subTotalCost + taxTotal + shipping;
-
-            cout << fixed << setprecision(2) << setw(10);
-            cout << "Chocolate Order #" << orderNum << endl;
-            cout << "Large Bars:  " << setw(4) << wantedLarge << setw(9) << wantedLarge * 5 << setw(12) << largeTotal << endl;
-            cout << "Medium Bars: " << setw(4) << wantedMedium << setw(9) << wantedMedium * 3 << setw(12) << mediumTotal << endl;
-            cout << "Small Bars:  " << setw(4) << wantedSmall << setw(9) << wantedSmall << setw(12) << smallTotal << endl;
-            cout << "Sub Total:  " << setw(14) << wantedLarge * 5 + wantedMedium * 3 + wantedSmall << setw(12) << subTotalCost << endl;
-            cout << "\nTax Total: " << setw(25) << taxTotal << endl;
-            cout << "Shipping: " << setw(25) << shipping << endl;
-            cout << "Total Cost: " << setw(25) << totalCost << endl << endl << endl;
-
-            orderNum++;
-            // break;
+            orderNum++; // Increases the order number by 1
         }
         else
         {
-            cout << "\nWARNING: Unable to fill order of " << orderNum << " point(s). We are " << numPounds - totalInv << " pounds short. We are sorry for the inconvenience." << endl;
+            cout << "\nWARNING: Unable to fill order #" << orderNum << ". We are " << numPounds - totalInv << " pounds short. We are sorry for the inconvenience." << endl;
         }
     }
     else
     {
         cout << "\nWARNING: Input is not a valid positive integer. Command ignored." << endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin.clear(); // Clears the error state of the user input object
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores a number of characters (in this case, the maximum size/amount/number) until it reaches the break character (in this case, a new line) from the user input object.
     }
-    // }
-}
-
-int command(char& choice, int& largeInv, int& mediumInv, int& smallInv, float largePrice, float mediumPrice, float smallPrice, int& orderNum) // I created this function to avoid too much clutter in the main function
-{
-    if (toupper(choice) == 'F')
-    {
-        fillOrder(largeInv, mediumInv, smallInv, largePrice, mediumPrice, smallPrice, orderNum);
-    }
-    else if (toupper(choice) == 'P')
-    {
-        printInventory(largeInv, mediumInv, smallInv);
-    }
-    else if (toupper(choice) == 'O')
-    {
-        orderInventory(largeInv, mediumInv, smallInv);
-    }
-    else if (toupper(choice) == 'E')
-    {
-        return 0;
-    }
-    else // If it's not one of the 4 choices above, it is most likely an unexpected input and thus an error
-    {
-        cout << "\nWARNING: " << choice << " is an invalid choice. Please try again." << endl;
-    }
-    return 1;
 }
 
 int main()
 {
     int orderNum = 1;
 
-    int largeInv = 1500;
-    int mediumInv = 1200;
-    int smallInv = 7000;
+    int largeInv = 620;
+    int mediumInv = 500;
+    int smallInv = 700;
 
-    float largePrice = 41.25;
-    float mediumPrice = 25.80;
-    float smallPrice = 13.50;
+    float largePrice = 27.99;
+    float mediumPrice = 21.99;
+    float smallPrice = 14.99;
 
-    char validChoices[4] = {'F', 'P', 'O', 'E'}; // Array that contains all valid choices for the command function
-    char choice;
+    char loop = 'Y';
 
-    int loop = 1; // 1 is true, 0 is false (Alternative solution to creating a boolean type loop variable)
-
-    while (loop == 1)
+    displayIntro();
+    while (toupper(loop) == 'Y')
     {
-        cout << "Chocolate Inventory\n" << "F: Fill Order\n" << "P: Print Inventory\n" << "O: Order Inventory\n" << "E: Exit\n" << "Enter your choice: ";
-        cin >> choice;
+        fillOrder(largeInv, mediumInv, smallInv, largePrice, mediumPrice, smallPrice, orderNum);
 
-        loop = command(choice, largeInv, mediumInv, smallInv, largePrice, mediumPrice, smallPrice, orderNum);
+        displayInventory(largeInv, mediumInv, smallInv);
+
+        cout << "Enter Y to process more orders: ";
+        cin >> loop;
 
         cout << endl;
     }
